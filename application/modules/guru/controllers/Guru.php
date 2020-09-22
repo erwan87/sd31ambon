cc89f5b708745e63fdd44f2170e4e38e48fdc62b
Erwan <erwan.wid@gmail.com>
2020-09-22 15:27:03 +0700

    Tambah Modul Guru, Jadwal Guru

---
+++ application/modules/guru/controllers/Guru.php
@@ -0,0 +1,98 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Guru extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->library('datatables');
+        $this->load->model('Guru_model');
+        $this->load->model('dashboard/Dashboard_model');
+    }
+    
+    public function index()
+    {
+        $id     = $this->session->userdata('id');
+        $data	= [
+            'titles'	=> "Dashboard Guru",
+            'user'	    => $this->Dashboard_model->viewu('tbl_users', 'id', $id)->result_array(),
+            'guru'	    => true,
+            'icons'     => "fa fa-home",
+            'breadcumb'	=> "Guru",
+            'view'		=> "v_guru"
+        ];
+        $this->load->view("index", $data);
+    }
+
+    // View Profile User
+    public function profuser($id = 0)
+    {
+        if ($id != 0) {
+            $data = [
+                'titles'		=> "Dashboard Administrator",
+                'user'	        => $this->Dashboard_model->viewu('tbl_users', 'id', $id)->result_array(),
+                'profile'		=> true,
+                'breadcumb'		=> "Profile",
+                'view'			=> "v_profuser"
+            ];
+            $this->load->view("index", $data);
+        }
+    }
+
+    // Edit Profile User
+    public function profedit()
+    {
+        $id	= $this->session->userdata('id');
+        
+        $this->form_validation->set_rules("nama_admin", "Nama_admin", "trim|min_length[5]|required");
+        if ($this->form_validation->run() == false) {
+            $data = [
+                'titles'		=> "Dashboard Administrator",
+                'user'  		=> $this->Siswa_model->viewprof($id)->result_array(),
+                'profile'		=> true,
+                'breadcumb'		=> "Profile",
+                'view'			=> "v_profuser"
+            ];
+            $this->load->view("index", $data);
+        } else {
+            $input = [
+                'name'	        => htmlspecialchars($this->input->post('nama_admin'))
+            ];
+            if ($this->Siswa_model->update_prof($id, $input)) {
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data User Berhasil di Update <br>Username : ' . $input['name'] . '</div>');
+            }
+            redirect('dashboard/profedit');
+        }
+    }
+
+    public function jadwal()
+    {
+        $id     = $this->session->userdata('id');
+        $data	= [
+            'titles'	=> "Dashboard Guru",
+            'user'	    => $this->Dashboard_model->viewu('tbl_users', 'id', $id)->result_array(),
+            'jadwal'    => true,
+            'icons'     => "fa fa-home",
+            'breadcumb'	=> "Jadwal",
+            'view'		=> "v_jadwal"
+        ];
+        $this->load->view("index", $data);
+    }
+
+    // Module About
+    public function about()
+    {
+        $id     = $this->session->userdata('id');
+        $data	= [
+            'titles'	=> "Dashboard Siswa",
+            'user'	    => $this->Dashboard_model->viewu('tbl_users', 'id', $id)->result_array(),
+            'About'	    => true,
+            'icons'     => "fa fa-info",
+            'breadcumb'	=> "About",
+            'view'		=> "v_about"
+        ];
+        $this->load->view("index", $data);
+    }
+}
---
+++ application/modules/guru/views/index.php
@@ -0,0 +1,8 @@
+<?php
+$this->load->view('template/v_headerdashboard');
+
+$this->load->view('template/v_sidebar');
+
+$this->load->view($view);
+
+$this->load->view('template/v_footerdashboard');
